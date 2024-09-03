9a3df814da0321dfecc58cbad0fd91f106dc67e9
kuzomix <yurisa331007@gmail.com>
2024-09-03 22:35:38 +0800

    24_09_03

--- Activity/app/src/main/java/com/example/activity/DetailedActivity.java
+++ Activity/app/src/main/java/com/example/activity/DetailedActivity.java
@@ -1,5 +1,6 @@
 package com.example.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,17 +9,31 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.activity.databinding.ActivityDetailedBinding;
+
 public class DetailedActivity extends AppCompatActivity {
+
+    ActivityDetailedBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_detailed);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        binding = ActivityDetailedBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Intent intent = this.getIntent();
+        if (intent != null){
+            String name = intent.getStringExtra("name");
+            String time = intent.getStringExtra("tima");
+            int ingredients = intent.getIntExtra("ingredients", R.string.maggiIngredients);
+            int desc = intent.getIntExtra("desc", R.string.maggieDesc);
+            int image = intent.getIntExtra("image", R.drawable.maggi);
+
+            binding.detailName.setText(name);
+            binding.detailTime.setText(time);
+            binding.detailDesc.setText(desc);
+            binding.detailIngredients.setText(ingredients);
+            binding.detailImage.setImageResource(image);
+        }
     }
 }
