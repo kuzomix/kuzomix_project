d0ebbf7164d66161590c16f6f06fbb85769556d0
kuzomix <yurisa331007@gmail.com>
2024-09-18 01:48:32 +0800

    09_18_0148

--- Application/app/src/main/java/com/example/application/Activity/BaseActivity.java
+++ Application/app/src/main/java/com/example/application/Activity/BaseActivity.java
@@ -1,22 +1,19 @@
-package com.example.application;
+package com.example.application.Activity;
 
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class BaseActivity extends AppCompatActivity {
-
+    FirebaseDatabase database;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
 
+        database=FirebaseDatabase.getInstance();
         Window w=getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
                 WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
--- Application/app/src/main/java/com/example/application/Activity/IntroActivity.java
+++ Application/app/src/main/java/com/example/application/Activity/IntroActivity.java
@@ -1,14 +1,7 @@
-package com.example.application;
+package com.example.application.Activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.example.application.databinding.ActivityIntroBinding;
 
@@ -21,7 +14,7 @@ public class IntroActivity extends BaseActivity {
         binding = ActivityIntroBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        binding.introBtn.setOnClickListener(view -> startActivity(new Intent(IntroActivity.this,MainActivity.class)));
+        binding.introBtn.setOnClickListener(view -> startActivity(new Intent(IntroActivity.this, MainActivity.class)));
 
     }
 }
