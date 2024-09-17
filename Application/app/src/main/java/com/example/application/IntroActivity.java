f3009212a6429128651001dc6ac2a6e59aad6dbd
kuzomix <yurisa331007@gmail.com>
2024-09-17 21:10:26 +0800

    09_17_0910

--- Application/app/src/main/java/com/example/application/IntroActivity.java
+++ Application/app/src/main/java/com/example/application/IntroActivity.java
@@ -1,6 +1,8 @@
 package com.example.application;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,13 +10,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.application.databinding.ActivityIntroBinding;
+
 public class IntroActivity extends BaseActivity {
+    ActivityIntroBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        binding = ActivityIntroBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_intro);
+
+        binding.introBtn.setOnClickListener(view -> startActivity(new Intent(IntroActivity.this,MainActivity.class)));
 
     }
 }
