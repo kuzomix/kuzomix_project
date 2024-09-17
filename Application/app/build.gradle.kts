8ad175e50294fae7b5dd8196e3a7f689b70ce64f
kuzomix <yurisa331007@gmail.com>
2024-09-17 17:25:26 +0800

    09_17_0525

--- Application/app/build.gradle.kts
+++ Application/app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -30,7 +31,7 @@ android {
         targetCompatibility = JavaVersion.VERSION_1_8
     }
     buildFeatures{
-        viewBinding
+        viewBinding = true
     }
 }
 
@@ -40,9 +41,10 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation 'com.github.ismaeldivite:chip-navigation-bar:1.4.0'
-    implementation 'androidx.viewpager2:viewpager2:1.0.0'
-    implementation 'com.github.bumptech.glide:glide:4.12.0'
+    implementation ("com.github.ismaeldivite:chip-navigation-bar:1.4.0")
+    implementation ("androidx.viewpager2:viewpager2:1.0.0")
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
+    implementation(libs.firebase.analytics)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
