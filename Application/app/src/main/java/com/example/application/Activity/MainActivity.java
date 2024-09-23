ac81e3ca09fb62936024b3674bcd91d74c1b3fee
kuzomix <yurisa331007@gmail.com>
2024-09-23 21:46:55 +0800

    24_09_23

--- Application/app/src/main/java/com/example/application/Activity/MainActivity.java
+++ Application/app/src/main/java/com/example/application/Activity/MainActivity.java
@@ -25,7 +25,7 @@ ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
 
